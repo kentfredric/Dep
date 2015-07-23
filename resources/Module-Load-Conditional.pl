47adc4b37ee17dc3b001dbd76619679ff9629463
Kent Fredric <kentnl@cpan.org>
2015-07-24 06:11:59 +1200

    Add Module-Load-Conditional

---
+++ resources/Module-Load-Conditional.pl
@@ -0,0 +1,26 @@
+item {
+    name 'KANE/Module-Load-Conditional-0.02.tar.gz';
+    provides 'Module::Load::Conditional' => '0.02';
+    requires 'Test::More';
+    requires 'Params::Check';
+    requires 'Module::Load';
+    requires 'Locale::Maketext::Simple';
+};
+item {
+    name 'KANE/Module-Load-Conditional-0.24.tar.gz';
+    provides 'Module::Load::Conditional' => '0.24';
+    requires 'Test::More';
+    requires 'Params::Check';
+    requires 'Module::Load' => '0.11';
+    requires 'Locale::Maketext::Simple';
+    requires 'version' => '0.69';
+};
+item {
+    name 'KANE/Module-Load-Conditional-0.26.tar.gz';
+    provides 'Module::Load::Conditional' => '0.26';
+    requires 'Test::More';
+    requires 'Params::Check';
+    requires 'Module::Load' => '0.11';
+    requires 'Locale::Maketext::Simple';
+    requires 'version' => '0.69';
+};
