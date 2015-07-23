3f8d634990219466ed08df2726498a74e9bbd58f
Kent Fredric <kentnl@cpan.org>
2015-07-24 04:03:21 +1200

    Add Archive-Tar table

---
+++ resources/Archive-Tar.pl
@@ -0,0 +1,8 @@
+item {
+    name 'GSAR/Archive-Tar-0.071.tar.gz';
+    provides 'Archive::Tar' => '0.071';
+};
+item {
+    name 'SRZ/Archive-Tar-0.22.tar.gz';
+    provides 'Archive::Tar' => '0.22';
+};
