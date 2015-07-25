b8253696db68d5809a286d29df620c284fabe2c9
Kent Fredric <kentnl@cpan.org>
2015-07-25 16:12:21 +1200

    Add libnet-1.00

---
+++ resources/libnet.pl
@@ -0,0 +1,12 @@
+item {
+    name 'GBARR/libnet-1.00.tar.gz';
+    provides 'Net::Cmd'    => '2.00';
+    provides 'Net::Domain' => '2.00';
+    provides 'Net::FTP'    => '2.00';
+    provides 'Net::NNTP'   => '2.00';
+    provides 'Net::Netrc'  => '2.00';
+    provides 'Net::POP3'   => '2.00';
+    provides 'Net::SMTP'   => '2.00';
+    provides 'Net::Telnet' => '2.00';
+    provides 'Net::Time'   => '2.00';
+};
