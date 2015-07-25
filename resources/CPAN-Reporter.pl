94669c3a37ec30829beb0e17c3816d9182c8b068
Kent Fredric <kentnl@cpan.org>
2015-07-25 17:32:03 +1200

    Fix missing dep on CPAN::Reporter

--- resources/CPAN-Reporter.pl
+++ resources/CPAN-Reporter.pl
@@ -1,8 +1,10 @@
 item {
     name 'DAGOLDEN/CPAN-Reporter-0.10.tar.gz';
     provides 'CPAN::Reporter' => '0.10';
+
     requires 'Config::Tiny'   => '2.08';
     requires 'File::HomeDir'  => '0.58';
     requires 'File::Temp'     => '0.16';
     requires 'Tee'            => '0.01';
+    requires 'Test::Reporter' => '1.27';
 };
