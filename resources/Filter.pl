6bfcf1d9b4a5c4e6db18de01cdc93dbafd0644d8
Kent Fredric <kentnl@cpan.org>
2015-07-25 19:13:14 +1200

    Add Filter

---
+++ resources/Filter.pl
@@ -0,0 +1,6 @@
+# First version to pass without XS failures on 5.6.1
+item {
+    name 'PMQS/Filter-1.16.tar.gz';
+    provides 'Filter::Util::Call' => '1.04';
+    provides 'Filter::Util::Exec' => '1.01';
+};
