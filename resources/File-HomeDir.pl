28f59c9c45c91844214122ec87590f1590d8cd71
Kent Fredric <kentnl@cpan.org>
2015-07-25 17:31:31 +1200

    File::HomeDir: Fix wrong author

--- resources/File-HomeDir.pl
+++ resources/File-HomeDir.pl
@@ -3,7 +3,7 @@ item {
     provides 'File::HomeDir' => '0.03';
 };
 item {
-    name 'SBURKE/File-HomeDir-0.58.tar.gz';
+    name 'ADAMK/File-HomeDir-0.58.tar.gz';
     provides 'File::HomeDir'       => '0.58';
     requires 'Test::More'          => '0.47';
     requires 'ExtUtils::MakeMaker' => '6.17';    # pm_to_blib sub
